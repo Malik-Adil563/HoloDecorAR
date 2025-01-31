d4bba8422561dce1d1f4e48fe429e4d6f305c9e3
adilm67 <163647730+adilm67@users.noreply.github.com>
2025-01-31 16:10:58 +0500

    Fix GLTFLoader import to include .js extension

--- src/Components/AppScene.js
+++ src/Components/AppScene.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { ARButton } from './ARButton.js'
 import 'webxr-polyfill';
 
