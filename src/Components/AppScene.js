4a873a0c0fec4af1db41b8712c9d705f6a2532fe
adilm67 <163647730+adilm67@users.noreply.github.com>
2025-02-17 18:39:53 +0500

    update productGallery

--- src/Components/AppScene.js
+++ src/Components/AppScene.js
@@ -88,7 +88,7 @@ const AppScene = ({ onClose }) => {
 
     const loader = new GLTFLoader();
     loader.load(
-      '/3DModels/chair.glb',
+      '/3DModels/chairs.glb',
       (gltf) => {
         model = gltf.scene;
         model.scale.set(0.01, 0.01, 0.01);
