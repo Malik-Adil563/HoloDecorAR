d375e8ade23638d5f5c957d8cbf9841fdd6bc5d8
Malik-Adil563 <madil4329@gmail.com>
2025-02-17 17:38:59 +0500

    Update AppScene.js

--- src/Components/AppScene.js
+++ src/Components/AppScene.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import 'webxr-polyfill';
 
 const AppScene = () => {
